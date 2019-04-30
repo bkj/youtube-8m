690a2bc73940975a62a771da90096684fb05f576
Ben Johnson <bkj.322@gmail.com>
2019-04-30 16:09:34 -0400

    fixing feature_extraction

--- feature_extractor.py
+++ feature_extractor.py
@@ -19,11 +19,11 @@ import tarfile
 import numpy
 from six.moves import urllib
 import tensorflow as tf
+import exline
 
 INCEPTION_TF_GRAPH = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
 YT8M_PCA_MAT = 'http://data.yt8m.org/yt8m_pca.tgz'
-MODEL_DIR = os.path.join(os.getenv('HOME'), 'yt8m')
-
+MODEL_DIR  = os.path.join(os.path.dirname(exline.__path__._path[0]), 'third_party/youtube-bm')
 
 class YouTube8MFeatureExtractor(object):
   """Extracts YouTube8M features for RGB frames.
